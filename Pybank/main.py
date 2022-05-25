633047528394f42b67f6bba2f6083cf9d2f1172f
Kinlay Denning <89169906+KinlayD@users.noreply.github.com>
2022-05-25 16:47:22 +1000

    Added summary stats to PyBank

--- Pybank/main.py
+++ Pybank/main.py
@@ -7,10 +7,24 @@ output_path = os.path.join("Resources", "budget_data.csv")
 
 with open(output_path) as x:
     budget_data = csv.DictReader(x, delimiter = ',')
+    profit_loss = []
+    for row_count, row in enumerate(budget_data):
+        value = int(row['Profit/Losses'])
+        profit_loss.append(value)
 
-    for row in budget_data:
-        print(row['Date'], row['Profit/Losses'])
-    
+print(f'Total Months: {row_count}')
+print(f'Net Total: {sum(profit_loss)}')
+print(f'Average Change in Profit: {sum(profit_loss)/row_count}')
+
+
+
+
+
+
+
+        
+
+
 
 
 
